30e059465b0eb3725fbbfc4c94b13c393eaba72f
Luke Davitt <luke.davitt1@gmail.com>
2016-05-13 08:25:17 -0400

    updates for examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples Component</h3>
-      <p>this is the examples page</p>
+      <h1 className="text-centered">Examples Component</h1>
+      <p>Here are a few example locations to try out</p>
+      <ol>
+        <li>
+          <Link to="/?location=Edgewater,FL">Edgewater, FL</Link>
+        </li>
+        <li>
+          <Link to="/?location=Indianapolis,IN">Indianapolis, IN</Link>
+        </li>
+      </ol>
     </div>
 )
 };
